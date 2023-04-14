7f7ad0db83198912d680a69b977c15e226e775b1
Mateus Santos <matesantos@hotmail.com>
2023-04-14 00:49:46 -0300

    projeto finalizado

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const form = document.querySelector("#novoItem");
 const ul = document.querySelector('#lista');
-itens = [];
+const itens = JSON.parse(localStorage.getItem('itens')) || [];
+
+itens.forEach(item => createElement(item));
 
 form.addEventListener('submit', evt => {
     evt.preventDefault();
@@ -8,29 +10,60 @@ form.addEventListener('submit', evt => {
     const nome = evt.target.elements['nome']
     const quantidade = evt.target.elements['quantidade'];
 
-    createElement(nome.value, quantidade.value);
+    const isPresent = itens.find(item => item.nome === nome.value);
+
+    const itemAtual = { 
+        "nome": nome.value, 
+        "quantidade": quantidade.value
+    }
+
+    if(isPresent){
+        itemAtual.id = isPresent.id
+        updateElement(itemAtual);
+        itens[itens.findIndex(elemento => elemento.id === isPresent.id)] = itemAtual
+    }else{
+        itemAtual.id = itens[itens.length -1] ? (itens[itens.length-1]).id +1 : 0
+        createElement(itemAtual);
+        itens.push(itemAtual)
+    } 
+    
+    localStorage.setItem('itens', JSON.stringify(itens));
     nome.value = "";
     quantidade.value = "";
 })
 
-function createElement(nome, quantidade) {
+function createElement(item) {
     const newItem = document.createElement('li');
     newItem.classList.add('item');
     
     const itemNumber = document.createElement('strong');
-    itemNumber.innerHTML = quantidade
+    itemNumber.innerHTML = item.quantidade
+    itemNumber.dataset.id = item.id;
     
     newItem.appendChild(itemNumber);
-    newItem.innerHTML += nome;
+    newItem.innerHTML += item.nome;
     
+    newItem.appendChild(buttonElement(item.id));
     ul.appendChild(newItem);
+}
 
-    const itemAtual = { 
-        "nome": nome, 
-        "quantidade": quantidade
-    }
+function updateElement(itemAtual){
+    const element = document.querySelector(`[data-id="${itemAtual.id}"]`);
+    element.innerHTML = itemAtual.quantidade; 
+}
 
-    itens.push(itemAtual)
+function buttonElement(id){
+    const buttonElement = document.createElement('button');
+    buttonElement.innerHTML = 'X';
+    buttonElement.addEventListener('click',function(){
+        deleteElement(this.parentNode, id)
+    })
+    return buttonElement;
+}
 
-    localStorage.setItem('item', JSON.stringify(itens));
+function deleteElement(tag, id){
+    tag.remove();
+    const index = itens.findIndex(element => element.id === id);
+    itens.splice(index,1);
+    localStorage.setItem("itens", JSON.stringify(itens));
 }
